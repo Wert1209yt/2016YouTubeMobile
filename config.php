aa2884c16e732231ba8c27068f4d4d0688bd9713
Wert1209yt <wert86022@gmail.com>
2025-10-11 16:41:12 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,3 @@
 <?php
-$invidious = "invidious.materialio.us"; //Insert your working invidious instance here. WITHOUT http:// or https:// and without / at the end!
 $yourip = "localhost:3000"; //Insert your IP or domain. WITHOUT http:// or https:// and without / at the end!
 ?>
